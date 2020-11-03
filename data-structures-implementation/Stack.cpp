aa213606a4ed1805a1bfd66ba2597c6017f413fd
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2020-11-04 00:11:23 +0530

    Updated Stack.cpp and added a few comments.

--- data-structures-implementation/Stack.cpp
+++ data-structures-implementation/Stack.cpp
@@ -7,29 +7,31 @@
 #include<string>
 
 using namespace std;
+// Created a class Stack and added its top and the size of array in which the stack will be implemented.
 
 class Stack{
 private:
     int top;
     int arr[5];
-
+// Added a public funtion for the Stack.
 public:
     Stack(){
-        top = -1;
-        for (int i = 0; i < 5; i++)
+        top = -1; // Initialised the top as -1 because the stack is currently emply.
+        for (int i = 0; i < 5; i++) 
         {
-            arr[i] = 0;
+            arr[i] = 0; // Used a for loop and Initialised all the elements of the Stack as 0.
         }
     }
 
+// Boolean Funtion isEmpty to check if the stack if empty or not.
     bool isEmpty(){
-    if (top == -1)
+    if (top == -1) // We check if the top is still -1 (since no value will be inserted). If its -1, then it will be empty.
     {
-        return true;
+        return true; // If yes it returns true
     }
     else
     {
-        return false;
+        return false; // Else it returns false
     }
     }
 
@@ -70,30 +72,31 @@ public:
         }
     }
 
-    int Count(){
+    int Count(){  // Count funtion which returns the number of elements in the Stack. It is simple and returns the top value + 1 since the stack is initialised from 0.
         return (top+1);
     }
 
-    int Peek(int p){
+    int Peek(int p){ // Peek function to used to return the element at a specific position p. It takes the input of position p and then returns the value at p.
     {
-        if(isEmpty())
+        if(isEmpty()) // If the stack is empty then it returns "Stack Underflow".
         {
             cout << "Stack Underflow";
             return 0;
         }
-        else
+        else  // Else it returns the value.
         {
-            return arr[p];
+            return arr[p]; 
         }
     }
     }
 
-    void Change(int p, int v)
+    void Change(int p, int v) // Change function which is used to change a specific value in the Stack. 
+                              // It expects 2 value, the position to be changed "p" and the value "v"
     {
         arr[p] = v;
     }
 
-    void Display()
+    void Display() // Display function to display all the elements in the stack.
     {
         cout << "The Values in this stack are: " << endl;
         for (int i = 4; i >= 0; i--)
@@ -106,7 +109,7 @@ public:
 // Main Program
 int main()
 {
-    Stack s;
+    Stack s; // Created an object s to use the Stack s.
 
 // Display Function to check if the stack works
     s.Display();
