5863035cd2b703978a4e835fbe60860120452c20
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2020-11-16 17:34:23 +0530

    Added A.cpp

---
+++ codeforces-contests/division-2/round-683/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define ll long long
+#define dbl double
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+		int n;
+		cin >> n;
+		cout <<n << endl;
+		for(int i = 1; i <= n; i++){
+			cout <<i <<" ";
+		}
+		cout << endl;
+    }
+}
